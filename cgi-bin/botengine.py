7c9e7b8e2006dd3665f6c13bc338022ac09dfd05
Hideyuki KOBAYASHI <kobayashi.hideyuki@leadinge.co.jp>
2019-07-20 15:28:36 +0900

    天気の情報が英語だったので日本語に変更

--- cgi-bin/botengine.py
+++ cgi-bin/botengine.py
@@ -15,7 +15,7 @@ apikey = "0b1f179a48ac27f22c849cb8088abb84"
 #cities = ["Tokyo,JP", "Kanagawa,JP", "Odawara,JP"]
 
 # APIのひな型 --- (※3)
-api = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}"
+api = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}&lang=ja&units=metric"
 
 # 温度変換( ケルビン→摂氏 ) --- (※4)
 k2c = lambda k: k - 273.15
@@ -86,10 +86,10 @@ def show_weather(city_name):
     # 結果はJSON形式なのでデコードする --- (※7)
     date = json.loads(r.text)
     # 結果を画面に表示 --- (※8)
-    sty = "今日の天気は" + date["weather"][0]["description"] + "。"
-    sty = sty + "最低気温は" +  str(k2c(date["main"]["temp_min"])) + "度。"
-    sty = sty + "最高気温は" +  str(k2c(date["main"]["temp_max"])) + "度。"
-    sty = sty + "湿度は" + str(date["main"]["humidity"]) + "％。"
+    sty = "今日の天気は " + date["weather"][0]["description"] + "。"
+    sty = sty + "最低気温は " +  str(date["main"]["temp_min"]) + "度。"
+    sty = sty + "最高気温は " +  str(date["main"]["temp_max"]) + "度。"
+    sty = sty + "湿度は " + str(date["main"]["humidity"]) + "％。"
     
     return sty
 
